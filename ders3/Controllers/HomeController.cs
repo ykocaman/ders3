3c1f6f2300548e85dbaaa43eb8ff29205f4116d7
ykocaman <yusuf.kocaman@msn.com>
2016-10-05 14:11:06 +0300

    init

---
+++ ders3/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ders3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.GirisBasarisiz = false;
+
+            if (Request.HttpMethod == "POST")
+            {
+                if (Request.Form["username"] == "yusuf" && Request.Form["password"] == "123")
+                {
+                    return Redirect("/Panel/Index");
+                }
+
+                ViewBag.GirisBasarisiz = true;
+            }
+            
+
+            return View();
+        }
+    }
+}
